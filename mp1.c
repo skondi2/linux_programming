64f00e0d210bffe55f1e36ec82ae66d2fd517394
skondi2 <skondi2@illinois.edu>
2022-09-21 09:53:17 -0500

    Seems to work

--- mp1.c
+++ mp1.c
@@ -1,10 +1,14 @@
 #define LINUX
 
+#include "mp1_given.h"
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
-#include "mp1_given.h"
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+#include <linux/workqueue.h>
+#include <linux/spinlock.h>
 #include "linux/list.h"
 
 MODULE_LICENSE("GPL");
@@ -17,10 +21,46 @@ MODULE_DESCRIPTION("CS-423 MP1");
 
 struct process_list {
    struct list_head list;
-   unsigned long pid;
+   int pid;
    unsigned long cpu_time;
 };
 struct process_list registered_processes;
+struct timer_list* timer;
+struct work_struct* work;
+spinlock_t lock;
+
+// ---------------------------------------------------------------------------------------------------------------
+void update_cpu_times(struct work_struct *work) {
+   //printk(KERN_ALERT "work function should be called every 5 seconds %lu\n", jiffies);
+
+   struct process_list* tmp;
+   struct list_head* curr_pos;
+   
+   spin_lock_irq(&lock);
+   list_for_each(curr_pos, &registered_processes.list) {
+      tmp = list_entry(curr_pos, struct process_list, list);
+      int pid = tmp->pid;
+
+      unsigned long new_cpu_time = 0;
+      int success = get_cpu_use((int)pid, &new_cpu_time);
+      if (success == -1) { // remove node from the list
+         list_del(curr_pos);
+         kfree(tmp);
+      }
+      else {
+         //printk("update_cpu_time(): new cpu time is %lu\n", new_cpu_time);
+         tmp->cpu_time = new_cpu_time;
+      }
+   }
+   spin_unlock_irq(&lock);
+
+   mod_timer(timer, jiffies + 5*HZ);
+}
+
+void timer_callback(struct timer_list* data) {
+   // puts the job into the kernel-global workqueue
+   schedule_work(work);
+}
 
 /*
    file: pointer to /mp1/status
@@ -31,43 +71,38 @@ struct process_list registered_processes;
    return the size of the buffer we filled
 */
 ssize_t proc_read_callback(struct file* file, char __user *buf, size_t size, loff_t* pos) {
-   /*if (*pos != 0) {
-      printk(KERN_ALERT "proc_read_callback(): pos offset != 0\n");
+   if (*pos != 0) { // CHECK: is it safe to just do this????
       return 0;
    }
    
-   char data[size + 1];
-   memset(buf_cpy, 0, size + 1);
+   char* data = kmalloc(size + 1, GFP_KERNEL);
+   memset(data, 0, size + 1); // CHECK: + 1 is correct????
+
    // write to data the linked list node data
    int bytes_read = 0;
    struct process_list* tmp;
    struct list_head* curr_pos;
+
+   spin_lock_irq(&lock);
    list_for_each(curr_pos, &registered_processes.list) {
       tmp = list_entry(curr_pos, struct process_list, list);
-      unsigned long pid = tmp->pid;
+      int pid = tmp->pid;
       unsigned long cpu_time = tmp->cpu_time;
 
-      int curr_bytes_read = sprintf(data + bytes_read, "%lu: %lu\n", pid, cpu_time);
-      if (curr_bytes_read <= 0) {
-         printk(KERN_ALERT "proc_read_callback(): Failed to copy node to data buffer\n");
-         return 0;
-      }
+      int curr_bytes_read = sprintf(data + bytes_read, "%d: %lu\n", pid, cpu_time);
       bytes_read += curr_bytes_read;
    }
+   spin_unlock_irq(&lock);
    data[bytes_read] = '\0';
 
-   int success = copy_to_user(buf, data, bytes_read);
+   int success = copy_to_user(buf, data, bytes_read + 1);
    if (success != 0) {
-      printk(KERN_ALERT "proc_read_callback(): Failed to copy data to user space\n");
+      //printk(KERN_ALERT "proc_read_callback(): Failed to copy data to user space\n");
       return 0;
    }
 
-   if (bytes_read != size) {
-      printk("proc_read_callback(): ")
-   }
-
+   *pos += bytes_read;
    return bytes_read; // return the number of bytes that were read
-   */
 }
 
 // ---------------------------------------------------------------------------------------------------------------
@@ -93,32 +128,29 @@ ssize_t proc_write_callback(struct file* file, const char __user *buf, size_t si
    buf_cpy[size] = '\0';
    if (success != 0) {
       printk(KERN_ALERT "proc_write_callback(): Failed to copy buf to kernel space\n");
-      return 0;
+      return -EFAULT;
    }
 
-   // convert pid to long
-   unsigned long pid = 0;
-   success = kstrtoul(buf_cpy, 10, &pid);
+   // convert pid to int
+   int pid = 0;
+   success = kstrtoint(buf_cpy, 10, &pid);
    if (success != 0) {
-      printk(KERN_ALERT "proc_write_callback(): Failed to convert pid to unsigned long\n");
-      return 0;
+      printk(KERN_ALERT "proc_write_callback(): Failed to convert pid to int\n");
+      return -EFAULT;
    }
-   //printk("proc_write_callback(): pid is %lu\n", pid);
 
    // get current cpu usage
    unsigned long cpu_time = 0;
-   success = get_cpu_use((int)pid, &cpu_time);
-   if (success != 0) {
-      printk(KERN_ALERT "proc_write_callback(): PID is invalid || CPU time not successfully returned\n");
-      return 0;
-   }
-   //printk("proc_write_callback(): cpu_time is %lu\n", cpu_time);
 
    // create new linked list node
    struct process_list* tmp = (struct process_list*) kmalloc(sizeof(struct process_list), GFP_KERNEL);
    tmp->pid = pid;
    tmp->cpu_time = cpu_time;
+   INIT_LIST_HEAD(&tmp->list);
+
+   spin_lock_irq(&lock);
    list_add_tail(&(tmp->list), &(registered_processes.list));
+   spin_unlock_irq(&lock);
 
    return size;
 }
@@ -144,8 +176,19 @@ int __init mp1_init(void)
       
    proc_dir = proc_mkdir("mp1", NULL);
    proc_file = proc_create("status", 0666, proc_dir, &proc_fops); 
-   
+
+   spin_lock_init(&lock);
+
    INIT_LIST_HEAD(&registered_processes.list);
+
+   timer = kmalloc(sizeof(struct timer_list), GFP_KERNEL);
+   timer_setup(timer, timer_callback, 0);
+   mod_timer(timer, jiffies + 5*HZ);
+
+   // update_cpu_times() is the function to be scheduled in the workqueue
+   // creates a workqueue with the name work
+   work = kmalloc(sizeof(struct work_struct), GFP_KERNEL);
+   INIT_WORK(work, update_cpu_times); 
 
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
    return 0;   
@@ -159,6 +202,14 @@ void __exit mp1_exit(void)
    #ifdef DEBUG
    printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
    #endif
+
+   del_timer_sync(timer);
+   kfree(timer);
+   
+   // terminates the work in the queue or blocks until the callback has finished 
+   // (if the work is already in progress in the handler)
+   cancel_work_sync(work);
+   kfree(work);
 
    struct process_list *tmp;
    struct list_head *pos, *q;
--- userapp.c
+++ userapp.c
@@ -1,6 +1,39 @@
 #include "userapp.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int fibonacci(int n) {
+	if (n == 0 || n == 1) return n;
+	return fibonacci(n - 2)  + fibonacci(n - 1);
+}
 
 int main(int argc, char* argv[])
 {
+	// write to /proc/mp1/status
+	FILE* ptr = fopen("/proc/mp1/status", "w");
+	if (ptr == NULL) {
+		printf("Trying to write, failed to open proc file\n");
+		return 1;
+	}
+	int pid = getpid();
+	fprintf(ptr, "%d", pid);
+	fclose(ptr);
+
+	// fibonacci sequence for large number
+	fibonacci(100);
+
+	// read from /proc/mp1/status
+	ptr = fopen("/proc/mp1/status", "r");
+	if (ptr == NULL) {
+		printf("Trying to read, failed to open proc file\n");
+		return 1;
+	}
+	char ch;
+	while (!feof(ptr)) {
+		ch = fgetc(ptr);
+		printf("%c", ch);
+	}
+	fclose(ptr);
+	
 	return 0;
 }
